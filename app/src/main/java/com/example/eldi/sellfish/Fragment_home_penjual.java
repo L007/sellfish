07288c2b233e171f0c4304966b71b600a0207d1f
L007 <antoeldi@gmail.com>
2017-10-23 15:29:24 +0700

    update info cuaca

--- app/src/main/java/com/example/eldi/sellfish/Fragment_home_penjual.java
+++ app/src/main/java/com/example/eldi/sellfish/Fragment_home_penjual.java
@@ -1,15 +1,40 @@
 package com.example.eldi.sellfish;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by eldi on 18/10/2017.
  */
 public class Fragment_home_penjual extends Fragment {
+    public static final String cuacaURL ="http://192.168.43.241/sellfish/cuaca.php";
+    String kota,cuaca,suhu,kelembaban;
+   // private ArrayList<String> cuaca;
+    TextView txtKota,txtCuaca,txtSuhu,txtKelembaban;
+
     public Fragment_home_penjual() {
 
     }
@@ -21,14 +46,86 @@ public class Fragment_home_penjual extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_penjual, container, false);
+        txtKota=(TextView)view.findViewById(R.id.kota);
+        txtCuaca=(TextView)view.findViewById(R.id.cuaca);
+        txtSuhu=(TextView)view.findViewById(R.id.suhu);
+        txtKelembaban=(TextView)view.findViewById(R.id.kelembaban);
+        tampilCuaca();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_penjual, container, false);
+        return view;
     }
-}
+
+
+    private void tampilCuaca() {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, cuacaURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jObj = new JSONObject(response);
+                            boolean error = jObj.getBoolean("error");
+                            if (!error) {
+                                //JSONObject user = jObj.getJSONObject("user");
+                                kota = jObj.get("kota").toString();
+                                cuaca = jObj.getString("cuaca").toString();
+                                suhu=jObj.getString("suhu").toString();
+                                kelembaban=jObj.getString("kelembaban").toString();
+
+                                txtKota.setText(kota);
+                                txtCuaca.setText(cuaca);
+                                txtSuhu.setText(suhu+" \u00b0"+"C");
+                                txtKelembaban.setText(kelembaban+" %");
+
+                            } else {
+                                // Error in login. Get the error message
+                                String errorMsg = jObj.getString("error_msg");
+                                Toast.makeText(getActivity(),
+                                        errorMsg, Toast.LENGTH_LONG).show();
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getActivity(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+
+
+                       /* if(response.trim().equals("success")){
+                            openProfile();
+                        }else{
+                            Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).show();
+                        }*/
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+
+        };
+        AppController.getInstance().addToRequestQueue(stringRequest);
+    }
+
+    }
+
+
+
 
